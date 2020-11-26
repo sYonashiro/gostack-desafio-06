abcdca98868d6ba27b89ae7d03ebbfa245dca1d4
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2020-11-26 00:46:24 -0300

    Ajustes no backend de acordo com desafio 07

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -2,9 +2,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -19,6 +21,9 @@ class Category {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 }
 
 export default Category;
